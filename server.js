ff53b6c91cd4abca9233e081ae9ff9b4b3b3dc97
Amir goodarzi <amirgoodarzi.web@gmail.com>
2022-09-11 21:12:39 +0430

    change port for prevent conflict between frontend and backend

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
   })
   .then(() => console.log('DB connection successful😎'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}😊`);
 });
